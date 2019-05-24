73063e03f4c0a1c5318cd302522c06295f717fd7
Toxicfy <15927630261@163.com>
2019-05-24 15:09:53 +0800

    :fire:  remove: 移除模拟登录功能

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const base64 = require('./utils/base64');
 const request = require('./utils/api');
 const Towxml = require('/towxml/main'); //引入towxml库
 
 //app.js
 App({
-    towxml: new Towxml(),
+    towxml: new Towxml(), //markdown解析渲染
+
     onLaunch: function() {
         this.getSystemInfo(); // 获取系统信息
-        this.testLogin(); // 模拟登陆
         this.getUserInfo(); // 获取当前登陆用户的基本信息
     },
+
     // 全局
     globalData: {
         userInfo: "",
@@ -25,14 +25,6 @@ App({
                 this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
             }
         });
-    },
-
-    // 模拟登陆
-    testLogin() {
-        const username = '';
-        const password = '';
-        let authorization = "Basic " + base64.base64_encode(username + ":" + password);
-        wx.setStorageSync('Authorization', authorization);
     },
 
     // 获取当前登陆用户的基本信息
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,74 +1,62 @@
 // pages/login/login.js
 const app = getApp();
+const base64 = require('../../utils/base64');
+
+// config
+const bgCoverUrl = 'https://cdn.pixabay.com/photo/2016/04/15/10/23/grindelwald-1330662_1280.jpg';
+const iconUrl = 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4250885442,2652050609&fm=26&gp=0.jpg';
+
 
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        StatusBar: app.globalData.StatusBar,
-        CustomBar: app.globalData.CustomBar,
-    },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function(options) {
-      setTimeout(() => {
-        return;
-        wx.switchTab({
-          url: '/pages/index/index',
-        })
-      },2000)
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function() {
-
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    bgCoverUrl,
+    iconUrl,
+  },
+  // 验证是否登录 
+  verifyLogin() {
+    let isLogin = !!wx.getStorageInfoSync("Authorization").keys.length
+    if (isLogin) {
+      wx.switchTab({
+        url: '/pages/index/index',
+      })
     }
+  },
+  // 点击登录
+  login: function(e) {
+    const {
+      username,
+      password
+    } = e.detail.value;
+
+    if (!!username && !!password) {
+      let authorization = "Basic " + base64.base64_encode(username + ":" + password);
+      wx.setStorageSync('Authorization', authorization);
+    }
+    this.verifyLogin();
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this.verifyLogin();
+  },
+
+  // 生命周期函数--监听页面初次渲染完成
+  onReady: function() {},
+  // 生命周期函数--监听页面显示
+  onShow: function() {},
+  // 生命周期函数--监听页面隐藏
+  onHide: function() {},
+  // 生命周期函数--监听页面卸载
+  onUnload: function() {},
+  // 页面相关事件处理函数--监听用户下拉动作
+  onPullDownRefresh: function() {},
+  // 页面上拉触底事件的处理函数
+  onReachBottom: function() {},
+  // 用户点击右上角分享
+  onShareAppMessage: function() {}
 })
