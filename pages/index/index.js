1d4aa9a075f8c9c58452abbfb18193694b7cc1ab
Toxicfy <15927630261@163.com>
2019-05-28 07:49:33 +0800

    :hammer: change: 重写获取/存储 repository 逻辑

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,39 +1,49 @@
-//index.js
+          //index.js
 const app = getApp();
-const request = require('../../utils/api');
-const utils = require('../../utils/util');
+const request = require("../../utils/api");
+const utils = require("../../utils/util");
 
 Page({
-    data: {
-        StatusBar: app.globalData.StatusBar,
-        CustomBar: app.globalData.CustomBar,
-        userInfo: app.globalData.userInfo,
-        articleInfo: "",
-    },
-    onLoad: function() {
-        wx.showLoading({
-                title: '数据加载中...',
-            })
-        // 获取 基本数据 and issues
-        request.get('/repos/toxicfy/Combing-notes/issues', {
-            per_page: 10,
-            page: 1
-        }).then(articleInfo => {
-            articleInfo.forEach(ele => {
-                ele.created_at = utils.formatTime(new Date(ele.created_at))
-            });
-            console.log(articleInfo);
-            this.setData({
-                articleInfo,
-                userInfo: app.globalData.userInfo
-            });
-            wx.hideLoading();
-        });
-    },
-    viewDetail(e) {
-        console.log(e.currentTarget.dataset.number);
-        wx.navigateTo({
-            url: `../details/details?number=${e.currentTarget.dataset.number}`
-        })
-    }
+  data: {
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    userInfo: app.globalData.userInfo,
+    articleInfo: ""
+  },
+
+  // 页面加载触发
+  onLoad: function() {
+    wx.showLoading({
+      title: "数据加载中..."
+    });
+    this.getIssues();
+  },
+
+  // 获取 baseInfo and issues
+  getIssues() {
+    const username = wx.getStorageSync("username");
+    const selectedRepository = wx.getStorageSync("selectedRepository");
+    const issuesUrl = `/repos/${username}/${selectedRepository}/issues`;
+
+    request.get(issuesUrl, { per_page: 10, page: 1 }).then(articleInfo => {
+      articleInfo.forEach(ele => {
+        ele.created_at = utils.formatTime(new Date(ele.created_at));
+      });
+
+      this.setData({
+        articleInfo,
+        userInfo: app.globalData.userInfo
+      });
+
+      wx.hideLoading();
+    });
+  },
+
+  // 获取单个 issues 详情
+  getViewDetail(e) {
+    console.log(e.currentTarget.dataset.number);
+    wx.navigateTo({
+      url: `../details/details?number=${e.currentTarget.dataset.number}`
+    });
+  }
 });
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,48 +1,51 @@
 // pages/login/login.js
 const app = getApp();
-const base64 = require('../../utils/base64');
-const request = require('../../utils/api')
+const base64 = require("../../utils/base64");
+const request = require("../../utils/api");
 
 // config
-const bgCoverUrl = 'https://cdn.pixabay.com/photo/2016/04/15/10/23/grindelwald-1330662_1280.jpg';
-const iconUrl = 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4250885442,2652050609&fm=26&gp=0.jpg';
-
+const bgCoverUrl =
+  "https://cdn.pixabay.com/photo/2016/04/15/10/23/grindelwald-1330662_1280.jpg";
+const iconUrl =
+  "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4250885442,2652050609&fm=26&gp=0.jpg";
 
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  // 页面的初始数据
   data: {
     bgCoverUrl,
     iconUrl,
+    repositoryList: [],
+    repositoryIndex: 0
   },
-  // 验证是否登录 
+  // 验证是否登录
   verifyLogin() {
     let isLogin = !!wx.getStorageInfoSync("Authorization").keys.length;
     if (isLogin) {
       wx.switchTab({
-        url: '/pages/index/index',
-      })
-      request.get('/user')
+        url: "/pages/index/index"
+      });
+      request
+        .get("/user")
         .then(res => {
           app.globalData.userInfo = res;
         })
         .catch(err => {
           this.handleErr(err);
-        })
+        });
     }
   },
   // 点击登录
   login: function(e) {
-    const {
-      username,
-      password
-    } = e.detail.value;
+    const { username, password } = e.detail.value;
+    const { repositoryList, repositoryIndex } = this.data;
+    // 设置当前用户以及选择的仓库
+    wx.setStorageSync("username", username);
+    wx.setStorageSync("selectedRepository", repositoryList[repositoryIndex]);
 
     if (!!username && !!password) {
-      let authorization = "Basic " + base64.base64_encode(username + ":" + password);
-      wx.setStorageSync('Authorization', authorization);
+      let authorization =
+        "Basic " + base64.base64_encode(username + ":" + password);
+      wx.setStorageSync("Authorization", authorization);
       //  进行验证
       this.getUserInfo();
     } else {
@@ -52,26 +55,53 @@ Page({
         duration: 2000
       });
     }
+  },
 
+  //获取 repository 列表
+  getRepository: function(e) {
+    const name = e.detail.value;
+    if (name) {
+      wx.request({
+        url: `https://api.github.com/users/${name}/repos`,
+        success: res => {
+          if (res.statusCode !== 200) return;
+          let temArr = [];
+          res.data.forEach(ele => {
+            temArr.push(ele.name);
+          });
+          this.setData({
+            repositoryList: temArr
+          });
+        }
+      });
+    }
+  },
+
+  // 捕获 repository 变化
+  repositoryChange: function(e) {
+    this.setData({
+      repositoryIndex: e.detail.value
+    });
   },
 
   // 获取当前登陆用户的基本信息
   getUserInfo() {
     wx.showLoading();
-    request.get('/user')
+    request
+      .get("/user")
       .then(res => {
         wx.hideLoading();
         app.globalData.userInfo = res;
         wx.switchTab({
-          url: '/pages/index/index',
-        })
+          url: "/pages/index/index"
+        });
       })
       .catch(err => {
         this.handleErr(err);
-      })
+      });
   },
 
-  // 
+  // 处理错误信息
   handleErr(err) {
     wx.hideLoading();
     wx.showToast({
@@ -79,7 +109,9 @@ Page({
       icon: "none",
       duration: 2000
     });
-    wx.removeStorageSync('Authorization');
+    wx.removeStorageSync("Authorization");
+    wx.setStorageSync("username");
+    wx.setStorageSync("selectedRepository");
   },
 
   // 生命周期函数--监听页面加载
@@ -101,4 +133,4 @@ Page({
   onReachBottom: function() {},
   // 用户点击右上角分享
   onShareAppMessage: function() {}
-})
+});
